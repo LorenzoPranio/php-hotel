e32d83a3e8dea71290db8421c99f1dfecd238666
LorenzoPranio <lorenzopranio02@gmail.com>
2024-09-05 15:56:28 +0200

    [UPD]Added info on screen without style

--- index.php
+++ index.php
@@ -47,9 +47,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>PHP Hotel</title>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <?php foreach ($hotels as $hotel){ ?>
+                    <?php echo $hotel['name'].'<br>' ?>
+                    <?php echo $hotel['description'].'<br>' ?>
+                    <?php echo $hotel['parking'] ? 'Sì'.'<br>' : 'No'.'<br>' ?>
+                    <?php echo $hotel['vote'].'<br>' ?>
+                    <?php echo $hotel['distance_to_center'].'<br>' ?>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
