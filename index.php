ff1c238cbc49d1713db68592a32f1caf3490aca4
LorenzoPranio <lorenzopranio02@gmail.com>
2024-09-05 16:55:02 +0200

    [UPD]Added style

--- index.php
+++ index.php
@@ -48,29 +48,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./style.css">
     <title>PHP Hotel</title>
 </head>
-<body>
+<body class="bg-darkbrown">
     <div class="container">
         <div class="row">
-            <table class="table">
-                <thead>
+            <div class="col-12 text-center">
+                <h1 class="my-4 fw-bolder goldtext">Hotels</h1>
+            </div>
+            <table class="table table-striped table-hover">
+                <thead class="bg-brown">
                     <tr>
-                        <th scope="col">Nome</th>
-                        <th scope="col">Descrizione</th>
-                        <th scope="col">Parcheggio</th>
-                        <th scope="col">Voto</th>
-                        <th scope="col">Distanza dal Centro</th>
+                        <th scope="col"><h6 class="goldtext fw-bolder m-0">Nome</h6></th>
+                        <th scope="col"><h6 class="goldtext fw-bolder m-0">Descrizione</h6></th>
+                        <th scope="col"><h6 class="goldtext fw-bolder m-0">Parcheggio</h6></th>
+                        <th scope="col"><h6 class="goldtext fw-bolder m-0">Voto</h6></th>
+                        <th scope="col"><h6 class="goldtext fw-bolder m-0">Distanza dal Centro</h6></th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody class="bg-lightbrown">
                     <?php foreach ($hotels as $hotel){ ?>
                         <tr>
                             <td><?php echo $hotel['name'] ?></td>
                             <td><?php echo $hotel['description'] ?></td>
                             <td><?php echo $hotel['parking'] ? 'Sì' : 'No' ?></td>
-                            <td><?php echo $hotel['vote'] ?></td>
-                            <td><?php echo $hotel['distance_to_center'] ?></td>    
+                            <td><?php echo $hotel['vote'].'/5'.'<i class="fa-solid fa-star"></i>' ?></td>
+                            <td><?php echo $hotel['distance_to_center'].'km' ?></td>    
                         </tr>
                     <?php } ?>
                 </tbody>
